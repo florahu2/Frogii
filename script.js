c66ff93cdd4cbef12f7376e5abef5191ad1a03df
Flora Hu <floratllh@gmail.com>
2021-08-27 23:38:27 -0400

    completed player

--- script.js
+++ script.js
@@ -18,8 +18,80 @@ loadSong(songs[songIndex])
 
 //Update song details 
 function loadSong(song) {
-    title.innerText = song 
+    title.innerText = '♫ ' + song 
     audio.src = `audio/${song}.mp3`
 }
 
+function playSong() {
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
 
+    audio.play()
+}
+
+function pauseSong() {
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+function prevSong() {
+    songIndex--
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1 
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function nextSong() {
+    songIndex++
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+    
+}
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement 
+    const progressPercent = (currentTime / duration) * 100 
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth 
+    const clickX = e.offsetX 
+    const duration = audio.duration 
+
+    audio.currentTime = (clickX / width) * duration
+}
+
+//Event Listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
+
+// Change song events
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress) 
