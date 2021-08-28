6e3a868721055f08aa484650a47fba06d011c383
Flora Hu <floratllh@gmail.com>
2021-08-27 22:23:08 -0400

    Added  html and css for music player

---
+++ script.js
@@ -0,0 +1,25 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn = document.querySelector('#play')
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.querySelector('#title') 
+
+//Song titles 
+const songs = ['Sunshine and Butterflies', 'Last Cup of Coffee', 'Foreverland']
+
+//Keep track of songs 
+let songIndex = 2 
+
+//Initially load song info 
+loadSong(songs[songIndex])
+
+//Update song details 
+function loadSong(song) {
+    title.innerText = song 
+    audio.src = `music/${song}.mp3`
+}
+
+
